ab1574966cf33aa0e29237765e16f8f3ee8f91cb
Eduardo Reche <edu.reche@gmail.com>
2015-09-11 19:27:41 -0300

    Added entry layout page

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,18 @@
-console.log('\'Allo \'Allo!');
+'use strict';
+
+angular.module('blogApp', ['firebase', 'ui.router'])
+	.config(function($stateProvider, $urlRouterProvider) {
+    	$urlRouterProvider.otherwise('/');
+
+    	$stateProvider//commons
+	      .state('home', { 
+	        url: '/',
+	        templateUrl: 'views/main.html'
+	      })
+	      .state('entry', {
+	      	url: '/entry',
+	      	templateUrl: 'views/entry.html'
+	      });
+
+	});
+
